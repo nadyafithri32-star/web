5d7e86ff873b28ec1160c86553bb263437484ef5
nadyafithri32-star <nadyafithri32@gmail.com>
2026-07-08 16:18:14 +0700

    Update Testimoni.tsx

--- src/components/Testimoni.tsx
+++ src/components/Testimoni.tsx
@@ -140,7 +140,7 @@ export default function Testimoni() {
         {/* Subtle background decoration */}
         <div className="position-absolute inset-0" style={{ opacity: 0.05, pointerEvents: "none" }}>
           <img
-            src="https://images.unsplash.com/photo-1517486808906-6ca8b3f04846?q=80&w=449&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://images.unsplash.com/photo-1517677129300-07b130802f46?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGdhbWJhciUyMG9yYW5nJTIwc2VueXVtfGVufDB8fDB8fHww"
             alt="Testimonials Banner"
             referrerPolicy="no-referrer"
             className="w-100 h-100"
